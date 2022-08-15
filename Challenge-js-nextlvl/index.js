5b654ae58be57a5d4e0bd26486f64f303ee52c31
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-15 22:53:53 +0200

    destructuring

--- Challenge-js-nextlvl/index.js
+++ Challenge-js-nextlvl/index.js
@@ -1,7 +1,38 @@
-/*
-Challenge: 
-1. Take the hard-coded HTML for the Wizard card, bring it 
-   into index.js and then inject it back into its div with 
-   JavaScript.
-2. Do the same for Orc card. 
-*/
+const hero ={
+  elementID:"hero",
+  namen:"hero",
+  avatar:"images/wizard.png",
+  health: 60,
+  diceN:[3,1,4],
+  diceCount:3
+}
+
+const monster ={
+  elementID: "monster",
+  namen: "monster",
+  avatar:"images/orc.png",
+  health: 10,
+  diceN: [2],
+  diceCount:1
+}
+
+
+function renderCharacter(data) {
+  const {elementID, namen, avatar, health, diceN, diceCount} = data
+  let diceHtml = ''
+  for (let i =0; i<diceCount; i++){
+    diceHtml += `<div class="dice">${diceN[i]}</div>`
+  }
+
+  document.getElementById(elementID).innerHTML=
+  `<div class="character-card">
+    <h4 class="name"> ${namen}</h4>
+    <img class="avatar" src="${avatar}"/>
+    <p class="health">health: <b> ${health} </b></p>
+    <div class="dice-container">${diceHtml} </div></div>
+
+  </div>`;
+}
+
+renderCharacter(hero)
+renderCharacter(monster)
