157d784eaf1642c10fa2679c77d51745db494ccb
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-16 11:19:03 +0200

    map, fill and function inside another

--- Challenge-js-nextlvl/index.js
+++ Challenge-js-nextlvl/index.js
@@ -3,7 +3,6 @@ const hero ={
   namen:"hero",
   avatar:"images/wizard.png",
   health: 60,
-  diceN:[3,1,4],
   diceCount:3
 }
 
@@ -12,17 +11,23 @@ const monster ={
   namen: "monster",
   avatar:"images/orc.png",
   health: 10,
-  diceN: [2],
   diceCount:1
 }
 
+function getDiceRollArray(diceCount){
+  const newArray = new Array(diceCount).fill(0)
+  return newArray.map(function(){
+    return Math.floor(Math.random()*6)+1})
+}
+
+function getDiceHtml(diceCount){
+     return getDiceRollArray(diceCount).map(number => `<div class="dice">${number}</div>`).join('');
+}
 
 function renderCharacter(data) {
-  const {elementID, namen, avatar, health, diceN, diceCount} = data
-  let diceHtml = ''
-  for (let i =0; i<diceCount; i++){
-    diceHtml += `<div class="dice">${diceN[i]}</div>`
-  }
+  const {elementID, namen, avatar, health, diceCount} = data
+  const diceHtml = getDiceHtml(diceCount)
+
 
   document.getElementById(elementID).innerHTML=
   `<div class="character-card">
