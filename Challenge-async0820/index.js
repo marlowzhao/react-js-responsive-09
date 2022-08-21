292ba2b5c9d652427cfcd6a012a062459f0c0dfa
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-21 11:01:01 +0200

    algorithm show

--- Challenge-async0820/index.js
+++ Challenge-async0820/index.js
@@ -1,27 +1,46 @@
-
 let deckId
-const button = document.getElementById("shuffle")
+const buttonShuffle = document.getElementById("shuffle")
 const buttonDraw = document.getElementById("draw")
 const placeCards= document.getElementById("card-load")
+const cardRemains= document.getElementById("card-rems")
+const cardWhoWins = document.getElementById("card-winner")
 
-button.addEventListener("click", handleClick)
+buttonShuffle.addEventListener("click", handleClick)
 function handleClick(){
   fetch("https://apis.scrimba.com/deckofcards/api/deck/new/shuffle/")
   .then(res=>res.json())
   .then(data=>{
-    console.log(data)
+    cardRemains.textContent = `Remaining Cards: ${data.remaining}`
+    // console.log(data)
     deckId = data.deck_id
   })
 }
 
-
 buttonDraw.addEventListener("click", ()=>{
-  fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
-  .then(res => res.json())
-  .then(data=> {
-    placeCards.children[0].innerHTML=`
-    <img class="insert" src=${data.cards[0].image} />`
-    placeCards.children[1].innerHTML=`
-    <img class="insert" src=${data.cards[1].image} />`
+    fetch(`https://apis.scrimba.com/deckofcards/api/deck/${deckId}/draw/?count=2`)
+    .then(res => res.json())
+    .then(data=> {
+            placeCards.children[0].innerHTML=`
+                <img class="insert" src=${data.cards[0].image} />`
+                placeCards.children[1].innerHTML=`
+                <img class="insert" src=${data.cards[1].image} />`
+            const winnerText = cardWinner(data.cards[0], data.cards[1])
+            cardWhoWins.textContent = winnerText
+
+            cardRemains.textContent = `Remaining Cards: ${data.remaining}`
+      })
   })
-})
+
+function cardWinner(cardOne, cardTwo){
+  const arrayCards =["2", "3", "4", "5", "6", "7", "8", "9", "10", "JACK", "QUEEN", "KING", "ACE"]
+  let indexOne = arrayCards.indexOf(cardOne.value)
+  let indexTwo = arrayCards.indexOf(cardTwo.value)
+
+  if (indexOne > indexTwo){
+    return "You win!"
+  }else if(indexTwo > indexOne){
+    return "Computer wins"
+  } else {
+    return "Draw game!"
+  }
+}
