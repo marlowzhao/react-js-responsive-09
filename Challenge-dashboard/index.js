56ee79b9214e1798b3fb1d766c6215bca62db2f7
jinsheng zhao <marlowevskyzhao@gmail.com>
2022-08-23 18:16:32 +0200

    temperature, location, time

--- Challenge-dashboard/index.js
+++ Challenge-dashboard/index.js
@@ -1,5 +1,5 @@
 
-  fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
+  fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=city")
   .then(res=>res.json())
   .then(data=>{
     const dataUrl = `url('${data.urls.regular}')`
@@ -20,13 +20,24 @@
       document.body.style.backgroundImage=`url("https://images.unsplash.com/photo-1427434973241-7f5dc143cfb5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxNDI0NzB8MHwxfHJhbmRvbXx8fHx8fHx8fDE2NjEyMDM0NDM&ixlib=rb-1.2.1&q=80&w=1080")`
     })
 
+  setInterval(() => {
+    const dDMmYy=new Date().toLocaleTimeString("en-us", {timeSytle: "short"})
+    document.getElementById("day-hour").textContent= dDMmYy
+  }, 1000);
 
-
-
-  // setInterval(() => {
-  //   const dDMmYy=new Date().toLocaleTimeString("en-us", {timeSytle: "short"})
-  //   document.getElementById("day-hour").textContent= dDMmYy
-  // }, 1000);
 navigator.geolocation.getCurrentPosition(position=>{
-  console.log(`${position.coords.latitude} and ${position.coords.longitude}`)
-})
+  fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}`)
+  .then(res=>{
+    if(!res.ok){
+      throw Error('weather data unavailable!')
+    }
+    return res.json()
+  })
+  .then(data=>{
+    // console.log(`${Math.round(data.main.temp)}`)
+    const weatherUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
+    document.getElementById("w-icon").innerHTML= `<img src=${weatherUrl}>`
+    document.getElementById("w-temperature").innerHTML=`<p> ${data.name} <br> ${data.weather[0].description}
+    <br>${Math.round(data.main.temp)}°</p>`
+    })
+});
